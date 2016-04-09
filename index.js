1e841058d00b24ba8bb579641ff9736c0db0d709
Jeremy Meyer <jjman505@gmail.com>
2016-04-09 16:53:44 -0400

    update index.js to use let & const

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var koa = require('koa');
-var app = koa();
+const koa = require('koa');
+const app = koa();
 
 // x-response-time
 app.use(function *(next) {
-  var start = new Date;
+  let start = new Date;
   yield next;
-  var ms = new Date - start;
+  let ms = new Date - start;
   this.set('X-Response-Time', ms + 'ms');
 });
 
 // logger
 app.use(function *(next) {
-  var start = new Date;
+  let start = new Date;
   yield next;
-  var ms = new Date - start;
+  let ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
