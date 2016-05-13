51668a34ff678fafc6d3f107344313dabf19d736
Jeremy Meyer <jjman505@gmail.com>
2016-05-13 03:22:07 -0400

    convert numbers with Number(x)

--- Tick.js
+++ Tick.js
@@ -3,14 +3,14 @@ module.exports = function Tick (obj) {
   if (Array.isArray(obj)) {
     return {
       currencyPair: obj[0],
-      timestamp:    obj[1],
-      bid:          obj[2],
-      bidPips:      obj[3],
-      offer:        obj[4],
-      offerPips:    obj[5],
-      high:         obj[6],
-      low:          obj[7],
-      open:         obj[8]
+      timestamp:    Number(obj[1]),
+      bid:          Number(obj[2]),
+      bidPips:      Number(obj[3]),
+      offer:        Number(obj[4]),
+      offerPips:    Number(obj[5]),
+      high:         Number(obj[6]),
+      low:          Number(obj[7]),
+      open:         Number(obj[8])
     };
   }
 
